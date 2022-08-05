b5026f11c095fa77ac749bc1ba1fbdd76485f449
Mathieu Le Tiec <mathieu.letiec+github@gmail.com>
2022-08-05 18:02:38 +0200

    Setup tests

---
+++ karma.conf.js
@@ -0,0 +1,18 @@
+module.exports = function (config) {
+  config.set({
+    browsers: ['ChromeHeadless'],
+    basePath: 'target',
+    files: ['karma-test.js'],
+    frameworks: ['cljs-test'],
+    plugins: [
+        'karma-cljs-test',
+        'karma-chrome-launcher',
+        'karma-junit-reporter'
+    ],
+    colors: true,
+    logLevel: config.LOG_INFO,
+    client: {
+      args: ['shadow.test.karma.init']
+    },
+  })
+}
---
+++ src/test/word_families/core_test.cljs
@@ -0,0 +1,6 @@
+(ns word-families.core-test
+  (:require [cljs.test :refer-macros [deftest testing is]]))
+
+(deftest fake-test
+  (testing "fake description"
+    (is (= 2 2))))
