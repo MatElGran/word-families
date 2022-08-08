94661ebf631d9040d984fc11e549b0bb726d3dcd
Mathieu Le Tiec <mathieu.letiec+github@gmail.com>
2022-08-08 21:12:04 +0200

    Setup CI witth GH actions

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,6 @@
 module.exports = function (config) {
   config.set({
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadlessNoSandbox'],
     basePath: 'target',
     files: ['karma-test.js'],
     frameworks: ['cljs-test'],
@@ -9,6 +9,12 @@ module.exports = function (config) {
         'karma-chrome-launcher',
         'karma-junit-reporter'
     ],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+      },
     colors: true,
     logLevel: config.LOG_INFO,
     client: {
