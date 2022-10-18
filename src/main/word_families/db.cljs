1c45c10f512dad7d355b50b8feb9382200ee24e7
Mathieu Le Tiec <mathieu.letiec+github@gmail.com>
2022-10-18 18:23:18 +0200

    Make settings control game creation

--- src/main/word_families/db.cljs
+++ src/main/word_families/db.cljs
@@ -1,33 +1,16 @@
 (ns word-families.db
   (:require
    [clojure.spec.alpha :as s]
-   [word-families.game.core :as game-core]
    [word-families.settings.core :as settings-core]
    [word-families.game.db :as game]
    [word-families.settings.db :as settings]))
-
-(defn- group->answers [group]
-  (let [group-name (::settings/name group)
-        members (::settings/members group)]
-    (zipmap members (repeat group-name))))
-
-(defn- groups->answers [groups]
-  (reduce
-   (fn [memo group]
-     (merge memo (group->answers group)))
-   {}
-   groups))
-
-(defn new-game [groups]
-  (let [expected-answers (groups->answers groups)]
-    (game-core/init expected-answers) ))
 
 ;; TODO: deserialize settings into a valid clojure structure (namespaced keys) or R/W edn ?
 (defn initial-db
   [user-settings]
   (let [settings (settings-core/init user-settings)]
     {::settings settings
-     ::current-game (new-game (::settings/groups settings))
+     ::current-game (settings-core/new-random-game settings)
      ;; FIXME: should be done according to path
      ::active-panel :home-panel}))
 
--- src/main/word_families/settings/core.cljs
+++ src/main/word_families/settings/core.cljs
@@ -1,6 +1,7 @@
 (ns word-families.settings.core
   (:require
-    [word-families.settings.db :as db]))
+   [word-families.game.core :as game]
+   [word-families.settings.db :as db]))
 
 (def default-groups [{::db/name "Terre"
                       ::db/members ["Enterrer" "Terrien" "Terrasse" "Terrier" "Extraterrestre" "Terrain" "Atterrir"]}
@@ -16,3 +17,21 @@
 (defn init [user-settings]
   (let [groups (or (::db/groups user-settings) default-groups)]
     {::db/groups groups}))
+
+(defn- group->answers [group]
+  (let [group-name (::db/name group)
+        members (::db/members group)]
+    (zipmap members (repeat group-name))))
+
+(defn- groups->answers [groups]
+  (reduce
+   (fn [memo group]
+     (merge memo (group->answers group)))
+   {}
+   groups))
+
+(defn- expected-answers [groups]
+  (groups->answers groups))
+
+(defn new-random-game [settings]
+  (game/init (expected-answers (::db/groups settings))))
