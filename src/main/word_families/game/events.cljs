c2f3052022a381e29c1779fb750ea40a84b67ebd
Mathieu Le Tiec <mathieu.letiec+github@gmail.com>
2022-10-18 16:35:09 +0200

    Introduce reg-event-game
    To register game-specific event handlers

--- src/main/word_families/game/events.cljs
+++ src/main/word_families/game/events.cljs
@@ -4,20 +4,18 @@
    [word-families.lib :as lib]
    [word-families.db :as db]))
 
-(lib/reg-event-db
+(lib/reg-event-game
  ::register-answer
  (fn-traced
-  [db [_ groupable group]]
-  (assoc-in db [::db/current-game ::db/answers groupable] group)))
+  [game [_ groupable group]]
+  (assoc-in game [::db/answers groupable] group)))
 
-(lib/reg-event-db
+(lib/reg-event-game
  ::validate-answers
  (fn-traced
-  [db _]
-  (let [actual-answers (get-in db [::db/current-game ::db/answers])
-        expected-answers (get-in db [::db/current-game ::db/expected-answers])
+  [game _]
+  (let [actual-answers (::db/answers game)
+        expected-answers  (::db/expected-answers game)
         error? (fn [answer] (not (= (get answer 1) (get expected-answers (get answer 0)))))
         errors (into {} (filter error? actual-answers))]
-    (-> db
-        (assoc-in [::db/current-game ::db/verified?] true)
-        (assoc-in [::db/current-game ::db/errors] errors)))))
+    (assoc game ::db/verified? true ::db/errors errors))))
--- src/main/word_families/lib.cljs
+++ src/main/word_families/lib.cljs
@@ -21,7 +21,17 @@
   ([id handler-fn]
    (rf/reg-event-fx id standard-interceptors-fx handler-fn))
   ([id interceptors handler-fn]
-   (rf/reg-event-fx
-    id
-    [standard-interceptors-fx interceptors]
-    handler-fn)))
+   (rf/reg-event-fx id [standard-interceptors-fx interceptors] handler-fn)))
+
+(defn game-handler->db-handler
+  [game-handler]
+  (fn [db event]
+    (let [current-game (::db/current-game db)
+          new-game (game-handler current-game event)]
+      (assoc db ::db/current-game new-game))))
+
+(defn reg-event-game
+  ([id handler-fn]
+   (reg-event-game id nil handler-fn))
+  ([id interceptors handler-fn]
+   (reg-event-db id [interceptors] (game-handler->db-handler handler-fn))))
