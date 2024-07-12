8eb4184107c68cda56391904ee89790689f029a4
Mathieu Le Tiec <mathieu.letiec+github@gmail.com>
2024-07-12 18:30:30 +0200

    Remove obsolete FIXME comment

--- src/main/word_families/core.cljs
+++ src/main/word_families/core.cljs
@@ -3,7 +3,6 @@
    [word-families.spec :as spec]
    [word-families.settings.core :as settings]))
 
-  ;; TODO: deserialize settings into a valid clojure structure (namespaced keys) or R/W edn ?
 (defn initial-db
   [user-settings route]
   (let [settings (settings/init user-settings)]
